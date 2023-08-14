777143a9d8ce2716195158aa5a2f2dd2b7850f77
Annefch <fchanne@163.com>
2023-08-15 00:05:10 +0800

    init

---
+++ src/src/webparts/reactspfx814/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IReactspfx814WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'Reactspfx814WebPartStrings' {
+  const strings: IReactspfx814WebPartStrings;
+  export = strings;
+}
